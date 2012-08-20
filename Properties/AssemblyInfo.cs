9fed5621295ebb87ebecfa2d56f9e8b9ffa59fed
Piotr Zurek <p.zurek@gmail.com>
2012-08-20 14:02:07 +1200

    Project and solution files for 2013

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Quick Tools for Revit")]
+[assembly: AssemblyDescription("A set of small useful tools for Revit 2013")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CADPRO Systems Ltd")]
+[assembly: AssemblyProduct("QuickTools")]
+[assembly: AssemblyCopyright("CADPRO Systems 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e3608d3b-101a-4029-b3a9-6de98d6717ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
